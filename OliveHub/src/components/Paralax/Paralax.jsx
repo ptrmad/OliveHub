5c6afa8cef3e1b862fdd7f83bf1611de072580ff
pmd <piotr.m.drozdzal@gmail.com>
2024-04-29 10:28:30 +0200

    Paralax working - to adjust

--- OliveHub/src/components/Paralax/Paralax.jsx
+++ OliveHub/src/components/Paralax/Paralax.jsx
@@ -1,5 +1,6 @@
-import OLIVESKY from "../../images/OliveSky.jpg";
-import OLIVEGARDEN from "../../images/OliveGarden.jpg";
+import LANDSCAPE1 from "../../images/Landscape1.png";
+import LANDSCAPE2 from "../../images/Landscape2.png";
+import LANDSCAPE3 from "../../images/Landscape3.png";
 import FRESHOLIVES from "../../images/FreshOlives.jpg";
 
 import styles from "./Paralax.module.css";
@@ -7,9 +8,11 @@ import styles from "./Paralax.module.css";
 export function Paralax({ children }) {
   return (
     <div className={styles.paralax}>
-      <img className={styles.sky} src={OLIVESKY} alt="" />
-      <img className={styles.ground} src={OLIVEGARDEN} alt="" />
+      <img className={styles.landscape3} src={LANDSCAPE3} alt="" />
+      <img className={styles.landscape2} src={LANDSCAPE2} alt="" />
+      <img className={styles.landscape1} src={LANDSCAPE1} alt="" />
       <img className={styles.front} src={FRESHOLIVES} alt="" />
+
       {children}
     </div>
   );
