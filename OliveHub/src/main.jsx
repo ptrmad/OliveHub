fc5130bd58cf77bce6fd05968ccbedbb9b431b83
pmd <piotr.m.drozdzal@gmail.com>
2024-04-29 13:49:47 +0200

    Paralax working, to merge with layout

--- OliveHub/src/main.jsx
+++ OliveHub/src/main.jsx
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./styles/globals.css";
 import "./styles/theme.css";
-// import { Layout } from "./components/Layout/Layout.jsx";
 import { NavBar } from "./components/NavBar/NavBar.jsx";
 import { Home } from "./views/Home/Home.jsx";
+import { Layout } from "./components/Layout/Layout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "",
-    element: <Home />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
   },
   {
     path: "test",
