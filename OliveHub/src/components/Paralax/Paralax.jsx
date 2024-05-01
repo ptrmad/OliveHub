0957fa38a39805c33fbcb6b08e25e99a4eaa9d55
pmd <piotr.m.drozdzal@gmail.com>
2024-05-01 12:49:28 +0200

    Paralax and olives working

--- OliveHub/src/components/Paralax/Paralax.jsx
+++ OliveHub/src/components/Paralax/Paralax.jsx
@@ -1,7 +1,7 @@
 import LANDSCAPE1 from "../../images/Landscape1.png";
 import LANDSCAPE2 from "../../images/Landscape2.png";
 import LANDSCAPE3 from "../../images/Landscape3.png";
-import OLIVES from "../../images/FreshOlives.jpg";
+// import OLIVES from "../../images/FreshOlives.jpg";
 
 import styles from "./Paralax.module.css";
 
@@ -11,16 +11,11 @@ export function Paralax() {
       <div className={styles.paralaxPlaceholder}></div>
 
       <div className={styles.paralax}>
-        <div className={styles.gradient}></div>
-
         <img className={styles.landscape3} src={LANDSCAPE3} alt="" />
         <img className={styles.landscape2} src={LANDSCAPE2} alt="" />
         <img className={styles.landscape1} src={LANDSCAPE1} alt="" />
 
         <div className={styles.olives}>
-          <div className={styles.gradient}></div>
-
-          {/* <img src={OLIVES} className={styles.olivesImg} /> */}
           <div className={styles.gradient}></div>
         </div>
       </div>
