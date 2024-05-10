bf0c24674777e675a11e30e618ee6340e76a365f
pmd <piotr.m.drozdzal@gmail.com>
2024-05-10 12:48:28 +0200

    Homepage and Ranking Links done

--- OliveHub/src/components/NewRev/NewRev.jsx
+++ OliveHub/src/components/NewRev/NewRev.jsx
@@ -1,6 +1,7 @@
 import styles from "./NewRev.module.css";
 import { data } from "../../data/data";
 import { ArticleCover } from "../ArticleCover/ArticleCover";
+import { Link } from "react-router-dom";
 
 export function NewRev() {
   return (
@@ -14,14 +15,15 @@ export function NewRev() {
                 .sort((a, b) => new Date(b.reviewDate) - new Date(a.reviewDate))
                 .slice(0, 1)
                 .map((item) => (
-                  <ArticleCover
-                    key={item.id}
-                    imgSource={item.image}
-                    isMain={true}
-                    oliveName={item.name}
-                    oliveBrand={item.brand}
-                    oliveRev={item.review.slice(0, 100)}
-                  />
+                  <Link key={item.itemId} to={`/${item.itemId}`}>
+                    <ArticleCover
+                      imgSource={item.image}
+                      isMain={true}
+                      oliveName={item.name}
+                      oliveBrand={item.brand}
+                      oliveRev={item.review.slice(0, 100)}
+                    />
+                  </Link>
                 ))}
             </div>
           </div>
@@ -30,14 +32,15 @@ export function NewRev() {
               .sort((a, b) => new Date(b.reviewDate) - new Date(a.reviewDate))
               .slice(1, 4)
               .map((item) => (
-                <ArticleCover
-                  key={item.id}
-                  imgSource={item.image}
-                  isMain={false}
-                  oliveName={item.name}
-                  oliveBrand={item.brand}
-                  oliveRev={item.review.slice(0, 100)}
-                />
+                <Link key={item.itemId} to={`/${item.itemId}`}>
+                  <ArticleCover
+                    imgSource={item.image}
+                    isMain={false}
+                    oliveName={item.name}
+                    oliveBrand={item.brand}
+                    oliveRev={item.review.slice(0, 100)}
+                  />{" "}
+                </Link>
               ))}
           </div>
         </div>
