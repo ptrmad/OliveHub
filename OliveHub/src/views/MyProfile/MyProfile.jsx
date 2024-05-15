b5dad3331e3d38f3230424306739d31f44dfca82
pmd <piotr.m.drozdzal@gmail.com>
2024-05-15 10:23:01 +0200

    MyProfile started

---
+++ OliveHub/src/views/MyProfile/MyProfile.jsx
@@ -0,0 +1,16 @@
+export function MyProfile() {
+  return (
+    <div>
+      <div>
+        <img />
+      </div>
+      <div>
+        profile data
+        <div>user id</div>
+        <div>number of favourites</div>
+        <div>date of registration</div>
+        <div>delete account</div>
+      </div>
+    </div>
+  );
+}
