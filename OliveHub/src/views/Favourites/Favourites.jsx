200ae472dfdda0abf6f667721bea32a118578359
pmd <piotr.m.drozdzal@gmail.com>
2024-05-15 09:48:19 +0200

    Favourites page styled like explore

--- OliveHub/src/views/Favourites/Favourites.jsx
+++ OliveHub/src/views/Favourites/Favourites.jsx
@@ -1,13 +1,31 @@
+import { Link } from "react-router-dom";
 import { FavouritesContext } from "../../contexts/FavouritesContext";
+import styles from "../Explore/Explore.module.css";
+
 import { use } from "react";
+import { ArticleCover } from "../../components/ArticleCover/ArticleCover";
 
 export function Favourites() {
   const { favourites } = use(FavouritesContext);
   return (
     <div>
-      <button onClick={() => console.log(favourites)}>
-        AAAAAAAAAAAAAAAAAAAAA
-      </button>
+      <div className={styles.listContainer}>
+        {favourites.map((item) => (
+          <div key={item.id} className={styles.list}>
+            <Link to={`/${item.itemId}`}>
+              <ArticleCover
+                isMain={false}
+                key={item.id}
+                imgSource={item.image}
+                oliveName={item.name}
+                oliveBrand={item.brand}
+                oliveRev={item.review.slice(0, 100)}
+                oliveRating={item.rating}
+              />
+            </Link>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
